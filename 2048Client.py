5dc0809b116af16ef15160fb0ebd25f505db4aba
Aiden Fertich <greyfertich@gmail.com>
2020-05-05 22:34:35 -0400

    Init game client

---
+++ 2048Client.py
@@ -0,0 +1,17 @@
+from PIL import ImageGrab, ImageOps
+import pyautogui
+
+LEFT, RIGHT, UP, DOWN = 1, 2, 3, 4
+
+class GameClient:
+    def __init__(self):
+        pass
+
+    def getBoard(self):
+        pass
+
+    def makeMove(self, direction):
+        """
+        Makes a move on the 2048 board in a direction (Left, Right, Up, Down)
+        """
+        pass

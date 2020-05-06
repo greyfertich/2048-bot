33fb9d19b3915bac38bbbfa984a8896f1f21253f
Aiden Fertich <greyfertich@gmail.com>
2020-05-05 22:43:16 -0400

    renamed client

---
+++ 2048_client.py
@@ -0,0 +1,16 @@
+from PIL import ImageGrab, ImageOps
+import pyautogui
+import directions
+
+class GameClient:
+    def __init__(self):
+        pass
+
+    def getBoard(self):
+        pass
+
+    def makeMove(self, direction):
+        """
+        Makes a move on the 2048 board in a direction (Left, Right, Up, Down)
+        """
+        pass
